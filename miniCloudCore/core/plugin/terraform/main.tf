6a029cecd046408cec972291074ed50b87a862f2
khero <m.laroui@esi-sba.dz>
2024-01-18 06:55:31 +0000

    terraform

--- miniCloudCore/core/plugin/terraform/main.tf
+++ miniCloudCore/core/plugin/terraform/main.tf
@@ -18,8 +18,8 @@ resource "virtualbox_vm" "node" {
   name      = format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/shekeriev/boxes/debian-11/versions/0.2/providers/virtualbox.box"
   cpus      = 1
-  memory    = "512 mib"
-  user_data = file("user_data")
+  memory    = "256 mib"
+  # user_data = file("user_data")
 
 
   network_adapter {
@@ -34,9 +34,9 @@ output "IPAddr" {
   value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
 }
 
-output "IPAddr_2" {
-  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
-}
+# output "IPAddr_2" {
+#   value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+# }
 
 
 
